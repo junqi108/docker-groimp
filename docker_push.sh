a52e8c3ac9665c3c33328379cb6e3abd7f7d848f
Junqi_Zhu <junqi108@gmail.com>
2022-11-22 14:37:06 +1300

    add the extraction of docker image id

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,8 @@
 
 . .env
 
+. .env
+
 ###################################################################
 # Main
 ###################################################################
@@ -9,5 +11,7 @@
 echo $CR_PAT | sudo docker login ghcr.io -u USERNAME --password-stdin
 
 # To find DOCKER_IMAGE_HASH: docker image ls
-docker tag "$DOCKER_IMAGE_HASH" "ghcr.io/${CONTAINER_REG_USER}/${CONTAINER_REG_NAME}:latest"
+IFS='\n' read -r -a array <<< "$(sudo docker images --filter=reference='debian:stable*' --format '{{.ID}}')"
+echo "${array[0]}"
+docker tag "${array[0]}" "ghcr.io/${CONTAINER_REG_USER}/${CONTAINER_REG_NAME}:latest"
 docker push "ghcr.io/${CONTAINER_REG_USER}/${CONTAINER_REG_NAME}:latest"
